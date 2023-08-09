f8137307eff4d74bbb98d43fbca02835946f6252
woofreeman <1872895@qq.com>
2023-08-10 00:13:53 +0800

    230810

---
+++ client.js
@@ -0,0 +1,15 @@
+const client = new Smallville({
+    host: "http://localhost:8080", // host of the server
+    stateHandler: function(state) {
+        //in here you would update the location of the agent using your own pathfinding algorithm
+        const agents = state.agents
+        const objects = state.locations
+        const conversations = state.conversations
+            
+        console.log('[State Change]: The simulation has been updated')
+},
+});
+client.createLocation({
+    name: 'Red House: Bathroom: Stove',
+    state: 'off'
+});
